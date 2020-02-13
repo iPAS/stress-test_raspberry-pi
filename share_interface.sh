f70b210004d7fd51eb8d20d22bfe1e9ca0da6427
iPAS <ptiwatthanont@gmail.com>
2020-02-13 14:17:13 +0700

    share_interface.sh | enable forwarding back to local

--- share_interface.sh
+++ share_interface.sh
@@ -2,20 +2,22 @@
 
 [[ $# -ne 2 ]] && echo "> $(basename $0) <LAN interface> <WLAN interface>" && exit 255
 
-IN=$1
-OUT=$2
+LAN=$1
+WLAN=$2
 
-echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward >> /dev/null
+#echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward >> /dev/null
+sudo sysctl -w net.ipv4.ip_forward=1
 
 sudo iptables -F
 sudo iptables -t nat -F
-sudo iptables -t mangle -F
+#sudo iptables -t mangle -F
 
-#sudo iptables -A POSTROUTING -o ${OUT} -t nat -j MASQUERADE
-#sudo iptables -A FORWARD -i ${OUT} -o ${IN} -m state --state RELATED,ESTABLISHED -j ACCEPT
-#sudo iptables -A FORWARD -i ${IN} -o ${OUT} -j ACCEPT
+#sudo iptables -A POSTROUTING -o ${WLAN} -t nat -j MASQUERADE
+#sudo iptables -A FORWARD -i ${WLAN} -o ${LAN} -m state --state RELATED,ESTABLISHED -j ACCEPT
+#sudo iptables -A FORWARD -i ${LAN} -o ${WLAN} -j ACCEPT
 
-sudo iptables -t nat -A POSTROUTING -o ${OUT} -j MASQUERADE
-sudo iptables -A FORWARD -i ${IN} -j ACCEPT
+sudo iptables -A FORWARD -i ${WLAN} -o ${LAN} -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i ${LAN} -j ACCEPT
+sudo iptables -t nat -A POSTROUTING -o ${WLAN} -j MASQUERADE
 
 sudo iptables -L -t nat
