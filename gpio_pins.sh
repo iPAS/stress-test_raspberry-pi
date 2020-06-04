9f870b3184e80edfd8772ae2540fb1b44d06018c
iPAS <ptiwatthanont@gmail.com>
2020-06-04 23:01:29 +0700

    Refactor

--- gpio_pins.sh
+++ gpio_pins.sh
@@ -1,4 +1,24 @@
 
 #poe_pin=34
 poe_pin=42
+rs485_pin=43
 
+function set_pin_mode {
+    pin=$1
+    mode=$2  # "out" or "in"
+
+    [[ ! -d /sys/class/gpio/gpio${pin} ]] && echo "${pin}" > /sys/class/gpio/export
+    echo ${mode} > /sys/class/gpio/gpio${pin}/direction
+}
+
+function set_pin_0 {
+    pin=$1
+
+    echo "0" > /sys/class/gpio/gpio${pin}/value
+}
+
+function set_pin_1 {
+    pin=$1
+
+    echo "1" > /sys/class/gpio/gpio${pin}/value
+}
--- gpio_poe_off.sh
+++ gpio_poe_off.sh
@@ -2,9 +2,5 @@
 
 source gpio_pins.sh
 
-[[ ! -d /sys/class/gpio/gpio${poe_pin} ]] && echo "${poe_pin}" > /sys/class/gpio/export
-echo "out" > /sys/class/gpio/gpio${poe_pin}/direction
-
-#echo "1" > /sys/class/gpio/gpio${poe_pin}/value
-echo "0" > /sys/class/gpio/gpio${poe_pin}/value
-
+set_pin_mode ${poe_pin} "out"
+set_pin_0 ${poe_pin}
